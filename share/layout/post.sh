667d674ee2233b5f3b788f0c0e06ab82ed88b5fd
hmngwy <hmngwy@users.noreply.github.com>
2016-12-31 01:51:51 +0800

    added tag index support

--- share/layout/post.sh
+++ share/layout/post.sh
@@ -58,6 +58,7 @@ cat << _EOF_
       <div class="heading"><a href="$(echo $POST_URL)"><span class="stamp">$(echo $POST_DATE)</span><h1 class="title">$(echo $POST_TITLE)</h1></a></div>
         <a href="/" class="home">←</a><div class="contents">
         $(echo $POST_CONTENTS)
+        $(for i in $TAGS; do echo "<a href=\"/tag/$i\">$i</a>"; done;)
         </div>
       </article>
     </body>
